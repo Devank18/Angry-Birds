c6fb2bf551330c6e74dab1b4ae18ce6fa14dbeb5
Devank18 <67155685+Devank18@users.noreply.github.com>
2020-10-04 12:32:29 +0530

    add Bird

--- sketch.js
+++ sketch.js
@@ -35,6 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    bird1 = new Bird(50,50);
 
 }
 
@@ -60,5 +61,6 @@ function draw(){
     log5.display();
 
     bird.display();
+    bird1.display(); 
     platform.display();
 }
